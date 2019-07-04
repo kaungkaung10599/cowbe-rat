9b5c667aa4c63788fb590da2c2fd67ba00598363
Kaung Htet Aung <donottouchme3@gmail.com>
2019-07-04 05:55:45 +0000

    download

--- download.js
+++ download.js
@@ -1,8 +1,9 @@
 var express  = require('express')
 var app  = express()
 var server = app.listen(8080, () => {
-    console.log('Server is started on 127.0.0.1:8081')
+    console.log('Server is started on 8080')
 })
 app.get('/downloadFile/', (req, res) => {
-  res.download('./demo.pdf');
+  	filepath = req.query.filepath;
+	res.download(filepath);
 })
