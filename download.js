79f32c93bf4379adab63a7b9b105d581fe8ea900
web3-motherfinance <tech_web3@motherfinance.com.mm>
2019-07-04 12:37:32 +0630

    download

--- download.js
+++ download.js
@@ -1,9 +1,10 @@
 var express  = require('express')
 var app  = express()
+var port = 8080
 var server = app.listen(8080, () => {
-    console.log('Server is started on 8080')
+    console.log('Server is started on ' + port)
 })
 app.get('/downloadFile/', (req, res) => {
-  	filepath = req.query.filepath;
-	res.download(filepath);
+  	filepath = req.query.filepath
+	res.download(filepath)
 })
